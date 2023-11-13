280eca01f1ff96bdbf6d6e58340efa9b7f01d60d
THANITPONG PATUMRATWORAKUL <tp.thanitpong@gmail.com>
2023-11-13 17:36:10 +0700

    [Add] Create View Pages and Components

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <>
+      <h1>HOME Protect</h1>
+    </>
+  )
+};
+
+export default Home;
---
+++ src/pages/admins/Admin.tsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+
+import { Outlet } from "react-router-dom"
+
+// type Props = {}
+
+const Admin = () => {
+  return (
+    <div>
+        Admin
+        <Outlet />
+    </div>
+  )
+}
+
+export default Admin
